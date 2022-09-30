15475e07b3f015e81e30b9c30d2996b59faee110
smith-garrett <gasmith@uni-potsdam.de>
2022-09-30 13:58:02 +0000

    Deploying to gh-pages from  @ 2f9e49698c35d0329ce2afc8b1242d13495c4808 🚀

---
+++ update_pubs.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# https://stackoverflow.com/questions/66600105/pandoc-citing-a-full-source
+pandoc --citeproc --csl=apa pubs_raw.md -t markdown-citations -o publications.md \
+    --lua-filter=/Users/garrettsmith/.pandoc/filters/inline_citation.lua
+
+# Remove unnecessary references section, kludge
+sed -i '' -e '/^:::/,$d' publications.md
